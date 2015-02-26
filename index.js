dfcfef1d2df71ddf4d2751dd398b5ec843d09ebb
Masayoshi Nomura <nmrmsys@gmail.com>
2015-02-26 17:33:24 +0900

    add naive implements

--- index.js
+++ index.js
@@ -5,31 +5,41 @@
 var program = require('commander');
 
 var fs = require('fs');
+var path = require('path');
 var converter = require('converter');
+
+var util = require('util');
+function dump(v){
+    return console.log(util.inspect(v));
+}
 
 program
     .version('0.0.1')
     .option('-a, --arg [Your arg]', 'Specify your arg.')
     .parse(process.argv);
 
-if(!program.arg) {
+files = program.args
+
+if(!files.length) {
     program.help();
 } else {
-  // get a file stream reader pointing to the csv file to convert 
-  var reader = fs.createReadStream('skeleton.xead');
-   
-  // get a file stream writer pointing to the json file to write to 
-  var writer = fs.createWriteStream('XEADSL.yaml');
-   
-  // setup the options for the data converter 
+
   var options = {
       from: 'xml',
       to: 'yml'
   };
-   
-  // get a data converter stream using the given options 
+  
   var convert = converter(options);
-   
-  // pipe everything to do the conversion 
-  reader.pipe(convert).pipe(writer);
+
+  for (i = 0; i < files.length; i++) {
+
+    from_file = files[i];
+    to_file = from_file.replace(new RegExp(path.extname(from_file)+'$'), '.yaml');
+
+    var reader = fs.createReadStream(from_file);
+    var writer = fs.createWriteStream(to_file);
+
+    reader.pipe(convert).pipe(writer);
+  }
+
 }
